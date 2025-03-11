7662e99324c22a6e1710a7e012735ca161cd9882
ksb-dev-1 <ksbabaleshwar@gmail.com>
2025-03-11 21:19:20 +0530

    add global stale and gc time

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -9,7 +9,7 @@ import ProgressBar from "@/components/ProgressBar";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+//import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +31,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         </Suspense>
         {children}
         <Toaster />
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </SessionProvider>
   );
